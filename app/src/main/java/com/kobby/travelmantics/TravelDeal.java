8aaabb211028e3161e6dd9d4bf79728f07f46876
= <=>
2019-08-03 19:00:56 +0000

    TravelMantics init commit. Has firbase setup with travel deals class and a simple save function

---
+++ app/src/main/java/com/kobby/travelmantics/TravelDeal.java
@@ -0,0 +1,67 @@
+package com.kobby.travelmantics;
+
+public class TravelDeal {
+    private String id;
+    private String aTitle;
+    private String aPrice;
+    private String aDescription;
+    private String aImageUrl;
+
+
+    public TravelDeal(String title, String price, String description,String imageUrl) {
+        this.id = id;
+        this.aTitle = title;
+        this.aPrice = price;
+        this.aDescription = description;
+        this.aImageUrl = imageUrl;
+    }
+
+    public String getImageUrl() {
+        return aImageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        aImageUrl = imageUrl;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return aTitle;
+    }
+
+    public void setTitle(String title) {
+        aTitle = title;
+    }
+
+    public String getPrice() {
+        return aPrice;
+    }
+
+    public void setPrice(String price) {
+        aPrice = price;
+    }
+
+    public String getDescription() {
+        return aDescription;
+    }
+
+    public void setDescription(String description) {
+        aDescription = description;
+    }
+
+
+
+
+
+
+
+
+}
